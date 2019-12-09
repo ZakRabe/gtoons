a95451175e5a3a8b10e6f5ef4ec55bbf515cd837
Zak Rabe <zakRabe@gmail.com>
2019-12-09 01:45:19 -0500

    added helmet

--- server/src/app.ts
+++
@@ -1,8 +0,0 @@
-import * as express from "express";
-import api from "./api";
-
-const app = express();
-app.set("port", process.env.PORT || 5000);
-api.init(app);
-
-export default app
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,38 @@
-import app from "./app";
-import socket from './socket'
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import * as helmet from "helmet";
+import { Request, Response } from "express";
 import "reflect-metadata";
+import { createConnection } from "typeorm";
+import { Routes } from "./routes";
+import socket from './socket';
 
-const http = require("http").Server(app);
-// set up socket.io and bind it to our
-// http server.
-const io = require("socket.io")(http);
+createConnection().then(async connection => {
 
-// whenever a user connects on port 5000 via
-// a websocket, log that a user has connected
-socket.init(io)
+  // create express app
+  const app = express();
+  app.use(helmet());
+  app.use(bodyParser.json());
+  const http = require("http").Server(app);
+  const io = require("socket.io")(http);
+  socket.init(io)
 
-const server = http.listen(5000, function () {
-  console.log("listening on *:5000");
-});
+  // register express routes from defined application routes
+  Routes.forEach(route => {
+    (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+      const result = (new (route.controller as any))[route.action](req, res, next);
+      if (result instanceof Promise) {
+        result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
+      } else if (result !== null && result !== undefined) {
+        res.json(result);
+      }
+    });
+  });
+
+  app.set("port", process.env.PORT || 5000);
+
+  const server = http.listen(5000, function () {
+    console.log("listening on *:5000");
+  });
+
+}).catch(error => console.log(error));
