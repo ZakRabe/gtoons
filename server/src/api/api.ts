bfe36f392837016577be4f19f430333dc5ba274c
Zak Rabe <zakRabe@gmail.com>
2019-12-08 23:06:06 -0500

    Add pgql typeorm

--- server/src/api/api.ts
+++ server/src/api/api.ts
@@ -1,10 +1,14 @@
-export const init = (app: any) => {
+import { Express, Request, Response } from 'express'
+
+export const init = (app: Express) => {
   // An api endpoint that returns a short list of items
-  app.get('/api/cards', (req: any, res: any) => {
+  app.get('/api/cards', (req: Request, res: Response) => {
     var list = [
       "card1", "card2", "card3"
     ];
     res.json(list);
     console.log('Sent list of cards');
   });
+
+
 }
--- server/src/models/User/User.ts
+++ server/src/models/User/User.ts
@@ -1,7 +1,7 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, Column, PrimaryGeneratedColumn, BaseEntity } from "typeorm";
 
 @Entity()
-export default class User {
+export default class User extends BaseEntity {
 
   @PrimaryGeneratedColumn()
   id: number;
@@ -14,5 +14,18 @@ export default class User {
 
   @Column()
   password: string;
+
+  @Column()
+  created: string;
+
+  toJson = () => {
+    // we don't want to expose user's email address or password via JSON
+    const { id, username, created } = this;
+    return {
+      id,
+      username,
+      created
+    }
+  }
 
 }
