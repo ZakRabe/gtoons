58e5b952e386b5c1c4debac7f032f207a11ac120
Zak Rabe <zakRabe@gmail.com>
2019-12-09 22:35:03 -0500

    Try this path

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,7 +7,7 @@ import { SocketProvider } from 'socket.io-react';
 import App from './App/App';
 import './index.css';
 
-const socket = io.connect({ path: '/api' });
+const socket = io.connect({ path: '/api/socket-io', port: "5000" });
 socket.on('message', (msg: any) => console.log(msg));
 
 ReactDOM.render((
