73f31d100587478e845751776c7f8d60c111bda3
Zak Rabe <zakRabe@gmail.com>
2019-12-11 00:39:49 -0500

    Remove dist from repo

--- server/dist/api/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const api = require("./api");
-exports.default = api;
-//# sourceMappingURL=index.js.map
--- server/dist/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const api_1 = require("./api");
-const app = express();
-app.set("port", process.env.PORT || 5000);
-api_1.default.init(app);
-exports.default = app;
-//# sourceMappingURL=app.js.map
