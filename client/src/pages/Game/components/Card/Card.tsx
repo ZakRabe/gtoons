e9b92e8335e5b1ddcb66cb14695d29f546f0b354
Zak Rabe <zakRabe@gmail.com>
2020-03-29 23:54:17 -0400

    THONKING

--- client/src/pages/Game/components/Card/Card.tsx
+++ client/src/pages/Game/components/Card/Card.tsx
@@ -2,18 +2,19 @@ import * as React from 'react';
 import CSS from 'csstype';
 
 const styles: CSS.Properties = {
-  position:'absolute',
-  top:'5px',
-  bottom:'5px',
-  left:'5px',
-  right:'5px',
-  backgroundColor:'red'
-}
+  position: 'absolute',
+  top: '5px',
+  bottom: '5px',
+  left: '5px',
+  right: '5px',
+  backgroundColor: 'red'
+};
 
 export default class Card extends React.Component<{}, {}> {
   render() {
     return (
       <div style={styles}>
+        <img style={{ width: '100%' }} src="/images/thonking.png" />
       </div>
     );
   }
