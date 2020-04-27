8ece6d86919c95eeee862c6fce527dc863e323ce
Zak Rabe <zakRabe@gmail.com>
2020-04-27 00:46:45 -0400

    UI Shell

--- client/src/App/AppHeader/AppHeader.tsx
+++ client/src/App/AppHeader/AppHeader.tsx
@@ -1,25 +1,110 @@
-import React from 'react';
+import {
+  Header,
+  HeaderGlobalAction,
+  HeaderGlobalBar,
+  HeaderMenuItem,
+  HeaderName,
+  HeaderNavigation,
+  HeaderPanel,
+  Switcher,
+  SwitcherItem,
+  SwitcherDivider,
+} from 'carbon-components-react/lib/components/UIShell';
+import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
-import { Divider, Header } from 'semantic-ui-react';
-import AppMenu from '../AppMenu';
+import { isLoggedIn, logOut } from '../../utils/auth';
+import { goto } from '../../utils/misc';
 import { AppHeaderProps } from './types';
+import './styles.css';
 
 const AppHeader: React.FunctionComponent<AppHeaderProps> = (props) => {
-  const goto = (path: string) => {
-    return () => {
-      const { history } = props;
-      history.push(path);
-    };
+  const { history } = props;
+
+  const [userMenuOpen, setUserMenuOpen] = useState(false);
+
+  const renderLoggedInMenu = () => {
+    return (
+      <>
+        <HeaderNavigation>
+          <HeaderMenuItem href="/lobbies">
+            <i className="fas fa-search"></i> &nbsp;Lobbies
+          </HeaderMenuItem>
+          <HeaderMenuItem href="/deckBuilder">
+            <i className="fas fa-tools"></i> &nbsp;Deck Builder
+          </HeaderMenuItem>
+        </HeaderNavigation>
+        <HeaderGlobalBar
+          style={{ fontSize: '1.25rem' }}
+          className="appHeaderActions"
+        >
+          <HeaderGlobalAction onClick={() => {}}>
+            <i className="fas fa-bell"></i>
+          </HeaderGlobalAction>
+
+          <HeaderGlobalAction
+            isActive={userMenuOpen}
+            onClick={() => setUserMenuOpen((prev) => !prev)}
+          >
+            <i className="fas fa-user-cog"></i>
+          </HeaderGlobalAction>
+
+          <HeaderGlobalAction
+            onClick={goto(history, 'https://discord.gg/W9Z9hSG')}
+          >
+            <i className="fab fa-discord"></i>
+          </HeaderGlobalAction>
+          <HeaderPanel expanded={userMenuOpen} className="profileMenu">
+            <Switcher>
+              <SwitcherItem onClick={() => {}}>
+                <i className="fas fa-user"></i>&nbsp; My Profile
+              </SwitcherItem>
+
+              <SwitcherItem
+                onClick={() => {
+                  logOut();
+                  goto(history, '/');
+                }}
+              >
+                <i className="fas fa-sign-out-alt"></i>&nbsp; Log out
+              </SwitcherItem>
+            </Switcher>
+          </HeaderPanel>
+        </HeaderGlobalBar>
+      </>
+    );
   };
+
+  const renderLoggedOutMenu = () => {
+    return (
+      <>
+        <HeaderNavigation>
+          <HeaderMenuItem href="/login">
+            <i className="fas fa-sign-in-alt"></i>&nbsp; Log In
+          </HeaderMenuItem>
+          <HeaderMenuItem href="/register">
+            <i className="fas fa-user-plus"></i>&nbsp; Sign Up
+          </HeaderMenuItem>
+        </HeaderNavigation>
+        <HeaderGlobalBar style={{ fontSize: '1.25rem' }}>
+          <HeaderGlobalAction
+            style={{ color: 'white' }}
+            onClick={goto(history, 'https://discord.gg/W9Z9hSG')}
+          >
+            <i className="fab fa-discord"></i>
+          </HeaderGlobalAction>
+        </HeaderGlobalBar>
+      </>
+    );
+  };
+
   const renderAppHeader = () => {
     return (
-      <header>
-        <Header as="h2" onClick={goto('/')} style={{ cursor: 'pointer' }}>
-          gToons Revived
-        </Header>
-        <Divider />
-        <AppMenu />
-      </header>
+      <Header className="appHeader">
+        <HeaderName href="/" prefix="">
+          reToons
+        </HeaderName>
+        {isLoggedIn() ? renderLoggedInMenu() : renderLoggedOutMenu()}
+      </Header>
     );
   };
 
--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -4,23 +4,14 @@ import {
   socketConnect,
   // @ts-ignore: no types for this
 } from 'socket.io-react';
+import { logOut } from '../../utils/auth';
 
 class Profile extends React.Component<{ socket: any }, {}> {
-  componentDidMount() {}
-
-  sendSocketMessage = () => {
-    const { socket } = this.props;
-    socket.emit('message', 'hello world');
-    socket.on('message', (msg: any) => console.log);
-  };
-
   render() {
     return (
       <>
         <Header as="h1">Profile</Header>
-        <button onClick={this.sendSocketMessage}>
-          Hello World to the server
-        </button>
+        <button onClick={logOut}>Log Out</button>
       </>
     );
   }
