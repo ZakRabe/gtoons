a3a466faef8ca6767dfbd771f18842f8b3c9039a
Anuj Asher <anujasher@gmail.com>
2020-07-25 22:07:26 +0530

    Remove request for card data

--- client/src/pages/DeckList/DeckList.tsx
+++ client/src/pages/DeckList/DeckList.tsx
@@ -1,20 +1,15 @@
 import { Button, Tile } from 'carbon-components-react';
-import CSS from 'csstype';
 import * as React from 'react';
-import { Card, Deck } from '../../App/types';
-import CardComponent from '../../components/Card';
+import { Deck } from '../../App/types';
 import { request } from '../../utils/api';
-import ColorButton from './components/ColorButton';
 import { DeckListProps } from './types';
 
 const DeckList: React.FunctionComponent<DeckListProps> = (props) => {
 
   const [decks, setDecks] = React.useState<Deck[]>([]);
-  const [allCards, setAllCards] = React.useState<Card[]>([]);
 
   React.useEffect(() => {
     request({ url: 'deckBuilder/myDeckList' }).then(setDecks);
-    request({ url: 'cards/all' }).then(setAllCards);
   }, []);
 
   const loadCards = (deckId: number | null) => {
@@ -28,36 +23,33 @@ const DeckList: React.FunctionComponent<DeckListProps> = (props) => {
     <section style={{ display: 'flex', flexDirection: 'row' }}>
       {decks.map((deck) => {
         const face = deck.face != null ? deck.face : deck.cards[Math.floor(Math.random() * deck.cards.length)];
-        const card = allCards.find((item: Card) => item.id === face) as Card;
-        if(card) {
-          return (
-            <section style={{ display: 'flex', flexDirection: 'column', padding: 20 }}>
-              <CardComponent model={card} width={250} height={250}/>
-              <Tile> {deck.name} </Tile>
-              <section style={{ display: 'flex', flexDirection: 'row' }}>
-                <Button
-                style={{ flex: 'auto' }}
-                renderIcon={() => (
-                  <i className="fas fa-pencil"/>
-                )}
-                onClick={() => loadCards(deck.id)}
-                >
-                Edit
-                </Button>
-                <Button
-                style={{ flex: 'auto' }}
-                renderIcon={() => (
-                  <i className="fas fa-trash"/>
-                )}
-                kind='danger'
-                disabled
-                >
-                Delete
-                </Button>
-              </section>
+        return (
+          <section style={{ display: 'flex', flexDirection: 'column', padding: 20 }}>
+            <img src={`/images/normal/released/${face}.jpg`} width="250" height="250"/>
+            <Tile> {deck.name} </Tile>
+            <section style={{ display: 'flex', flexDirection: 'row' }}>
+              <Button
+              style={{ flex: 'auto' }}
+              renderIcon={() => (
+                <i className="fas fa-pencil"/>
+              )}
+              onClick={() => loadCards(deck.id)}
+              >
+              Edit
+              </Button>
+              <Button
+              style={{ flex: 'auto' }}
+              renderIcon={() => (
+                <i className="fas fa-trash"/>
+              )}
+              kind='danger'
+              disabled
+              >
+              Delete
+              </Button>
             </section>
-          )
-        }
+          </section>
+        )
       })}
     </section>
     <section>
