3afd7b425b5968747211456974b60f2707774b81
WLSN <>
2020-09-10 14:02:02 -0400

    Continued work on check function Added first draft of adjacent and neighbor condition restrictions

--- server/src/powers/utils.ts
+++ server/src/powers/utils.ts
@@ -57,6 +57,7 @@ function check(p1Cards: (Card | null)[], p2Cards: (Card | null)[], power: any) {
   //Check if condition is position based
   let powerID = power.conditions.source;
   let powerPosition = -1;
+  let isP1Power = false;
   // Check Player one cards for conditional matches
   p1Cards.map((card) => {
     if (card) {
@@ -77,6 +78,18 @@ function check(p1Cards: (Card | null)[], p2Cards: (Card | null)[], power: any) {
             // Check if ARRAY based (colors, groups, types) or SINGLE (character)
             if (Array.isArray(card[condition.attribute])) {
               if (card[condition.attribute].indexOf(condition.value) > -1) {
+                powerPosition = p1Cards.findIndex((card) => {
+                  card.id == powerID;
+                });
+                isP1Power = powerPosition > -1;
+
+                // Check if it is p1 or p2 power
+                if (!isP1Power) {
+                  // If not a p1 power, get position of power in p2 cards
+                  powerPosition = p2Cards.findIndex((card) => {
+                    card.id == powerID;
+                  });
+                }
                 switch (power.conditionType) {
                   case 'NONE':
                     // Nothing needs to be done.
@@ -85,29 +98,228 @@ function check(p1Cards: (Card | null)[], p2Cards: (Card | null)[], power: any) {
                   case 'ADJACENT':
                     // Check cards positions adjacent to this powers card
                     // Also checks neighbors?
-                    //check if p1 or p2 power
-                    powerPosition = p1Cards.findIndex((card) => {
-                      card.id == powerID;
-                    });
-                    if (powerPosition > -1) {
+                    if (isP1Power) {
                       /*
                       Check +3,+4,-3,-4
                       Clamps: Can't be less than 0 or more than 6
-                      Position 1 [0]: excludes +3,-3,-4
-                      Position 2 [1]: excludes -3,-4
-                      Position 3 [2]: excludes -3,-4
-                      Position 4 [3]: excludes +4,-3,-4
-                      Position 5 [4]: excludes +3,+4
-                      Position 6 [5]: excludes +3,+4
-                      Position 7 [6]: excludes +3,+4
+                      Power position 1 [0]: includes +4
+                      Power position 2 [1]: includes +3,+4
+                      Power position 3 [2]: includes +3,+4
+                      Power position 4 [3]: includes +3
+                      Power position 5 [4]: includes -3,-4
+                      Power position 6 [5]: includes -3,-4
+                      Power position 7 [6]: includes -3,-4
                        */
-                    } else {
-                      powerPosition = p2Cards.findIndex((card) => {
-                        card.id == powerID;
-                      });
+                      switch (powerPosition) {
+                        case 0:
+                          if (
+                            powerPosition + 4 ===
+                            p1Cards.findIndex((_card) => {
+                              _card.id === card.id;
+                            })
+                          ) {
+                            matching = true;
+                          }
+                          break;
+                        case 1:
+                        case 2:
+                          if (
+                            powerPosition + 3 ===
+                              p1Cards.findIndex((_card) => {
+                                _card.id === card.id;
+                              }) ||
+                            powerPosition + 4 ===
+                              p1Cards.findIndex((_card) => {
+                                _card.id === card.id;
+                              })
+                          ) {
+                            matching = true;
+                          }
+                          break;
+                        case 3:
+                          if (
+                            powerPosition + 3 ===
+                            p1Cards.findIndex((_card) => {
+                              _card.id === card.id;
+                            })
+                          ) {
+                            matching = true;
+                          }
+                          break;
+                        case 4:
+                        case 5:
+                        case 6:
+                          if (
+                            powerPosition - 3 ===
+                              p1Cards.findIndex((_card) => {
+                                _card.id === card.id;
+                              }) ||
+                            powerPosition - 4 ===
+                              p1Cards.findIndex((_card) => {
+                                _card.id === card.id;
+                              })
+                          ) {
+                            matching = true;
+                          }
+                          break;
+                      }
+                    } // p2 Power
+                    else {
+                      /*
+                      Check +3,+4,-3,-4
+                      Clamps: Can't be less than 0 or more than 6
+                      Power position 1 [0]: includes +4
+                      Power position 2 [1]: includes +3,+4
+                      Power position 3 [2]: includes +3,+4
+                      Power position 4 [3]: includes +3
+                      Power position 5 [4]: includes -3,-4
+                      Power position 6 [5]: includes -3,-4
+                      Power position 7 [6]: includes -3,-4
+                       */
+                      switch (powerPosition) {
+                        case 0:
+                          if (
+                            powerPosition + 4 ===
+                            p2Cards.findIndex((_card) => {
+                              _card.id === card.id;
+                            })
+                          ) {
+                            matching = true;
+                          }
+                          break;
+                        case 1:
+                        case 2:
+                          if (
+                            powerPosition + 3 ===
+                              p2Cards.findIndex((_card) => {
+                                _card.id === card.id;
+                              }) ||
+                            powerPosition + 4 ===
+                              p2Cards.findIndex((_card) => {
+                                _card.id === card.id;
+                              })
+                          ) {
+                            matching = true;
+                          }
+                          break;
+                        case 3:
+                          if (
+                            powerPosition + 3 ===
+                            p2Cards.findIndex((_card) => {
+                              _card.id === card.id;
+                            })
+                          ) {
+                            matching = true;
+                          }
+                          break;
+                        case 4:
+                        case 5:
+                        case 6:
+                          if (
+                            powerPosition - 3 ===
+                              p2Cards.findIndex((_card) => {
+                                _card.id === card.id;
+                              }) ||
+                            powerPosition - 4 ===
+                              p2Cards.findIndex((_card) => {
+                                _card.id === card.id;
+                              })
+                          ) {
+                            matching = true;
+                          }
+                          break;
+                      }
                     }
                   case 'NEIGHBOR':
-                    // Check positions next to this powers card
+                    if (isP1Power) {
+                      /*
+                      Check +1,-1
+                      Clamps: Can't be less than 0 or more than 6
+                      Power position 1 [0]: includes +1
+                      Power position 4 [3]: includes -1
+                      Power position 5 [4]: includes +1
+                      Power position 7 [6]: includes -1
+                       */
+                      switch (powerPosition) {
+                        case 0:
+                        case 4:
+                          if (
+                            powerPosition + 1 ===
+                            p1Cards.findIndex((_card) => {
+                              _card.id === card.id;
+                            })
+                          ) {
+                            matching = true;
+                          }
+                          break;
+                        case 3:
+                        case 6:
+                          if (
+                            powerPosition - 1 ===
+                            p1Cards.findIndex((_card) => {
+                              _card.id === card.id;
+                            })
+                          ) {
+                            matching = true;
+                          }
+                          break;
+                        default:
+                          if (
+                            powerPosition + 1 ===
+                              p1Cards.findIndex((_card) => {
+                                _card.id === card.id;
+                              }) ||
+                            powerPosition - 1 ===
+                              p1Cards.findIndex((_card) => {
+                                _card.id === card.id;
+                              })
+                          ) {
+                            matching = true;
+                          }
+                          break;
+                      }
+                    }
+                    // p2 Power
+                    else {
+                      switch (powerPosition) {
+                        case 0:
+                        case 4:
+                          if (
+                            powerPosition + 1 ===
+                            p2Cards.findIndex((_card) => {
+                              _card.id === card.id;
+                            })
+                          ) {
+                            matching = true;
+                          }
+                          break;
+                        case 3:
+                        case 6:
+                          if (
+                            powerPosition - 1 ===
+                            p2Cards.findIndex((_card) => {
+                              _card.id === card.id;
+                            })
+                          ) {
+                            matching = true;
+                          }
+                          break;
+                        default:
+                          if (
+                            powerPosition + 1 ===
+                              p2Cards.findIndex((_card) => {
+                                _card.id === card.id;
+                              }) ||
+                            powerPosition - 1 ===
+                              p2Cards.findIndex((_card) => {
+                                _card.id === card.id;
+                              })
+                          ) {
+                            matching = true;
+                          }
+                          break;
+                      }
+                    }
                     break;
                   case 'OPPOSING':
                     break;
