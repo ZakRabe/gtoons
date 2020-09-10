aa871ecb2be813d1bd0944a393cf7164f7984d67
WLSN <>
2020-09-10 00:17:01 -0400

    Working on the check function for the powers util. Still in a very rough-draft

--- server/src/powers/utils.ts
+++ server/src/powers/utils.ts
@@ -46,8 +46,110 @@ export function evaluateBoardPowers(
     if (card) {
       card.powers.map((power) => {
         //Switch Case for each power type? SINGLE or FOR_EACH
-        console.log(power);
+        //console.log(power);
+        check(p1Cards, p1Cards, power);
       });
     }
   });
 }
+
+function check(p1Cards: (Card | null)[], p2Cards: (Card | null)[], power: any) {
+  //Check if condition is position based
+  let powerID = power.conditions.source;
+  let powerPosition = -1;
+  // Check Player one cards for conditional matches
+  p1Cards.map((card) => {
+    if (card) {
+      let mustMatchAll = power.conditionType === 'ALL';
+      let matching = false;
+      let matchingAll = true;
+      // Need to check array position for nextTo, adjacent, opposing, etc..
+      power.conditions.map((condition) => {
+        // Check if attribute is position or card attribute
+        if (condition.attribute !== 'position') {
+          // Checking IS or IS_NOT
+          if (condition.condition === 'IS') {
+          } else {
+          }
+        } else {
+          if (condition.condition === 'IS') {
+            // Checking IS or IS_NOT
+            // Check if ARRAY based (colors, groups, types) or SINGLE (character)
+            if (Array.isArray(card[condition.attribute])) {
+              if (card[condition.attribute].indexOf(condition.value) > -1) {
+                switch (power.conditionType) {
+                  case 'NONE':
+                    // Nothing needs to be done.
+                    matching = true;
+                    break;
+                  case 'ADJACENT':
+                    // Check cards positions adjacent to this powers card
+                    // Also checks neighbors?
+                    //check if p1 or p2 power
+                    powerPosition = p1Cards.findIndex((card) => {
+                      card.id == powerID;
+                    });
+                    if (powerPosition > -1) {
+                      /*
+                      Check +3,+4,-3,-4
+                      Clamps: Can't be less than 0 or more than 6
+                      Position 1 [0]: excludes +3,-3,-4
+                      Position 2 [1]: excludes -3,-4
+                      Position 3 [2]: excludes -3,-4
+                      Position 4 [3]: excludes +4,-3,-4
+                      Position 5 [4]: excludes +3,+4
+                      Position 6 [5]: excludes +3,+4
+                      Position 7 [6]: excludes +3,+4
+                       */
+                    } else {
+                      powerPosition = p2Cards.findIndex((card) => {
+                        card.id == powerID;
+                      });
+                    }
+                  case 'NEIGHBOR':
+                    // Check positions next to this powers card
+                    break;
+                  case 'OPPOSING':
+                    break;
+                  case 'OPPONENT':
+                    break;
+                }
+              } else {
+                matchingAll = false;
+              }
+            } else {
+              if (card[condition.attribute] === condition.value) {
+                matching = true;
+              } else {
+                matchingAll = false;
+              }
+            }
+          } else {
+            // Check if ARRAY based (colors, groups, types) or SINGLE (character)
+            if (Array.isArray(card[condition.attribute])) {
+              if (card[condition.attribute].indexOf(condition.value) == -1) {
+                matching = true;
+              } else {
+                matchingAll = false;
+              }
+            } else {
+              if (card[condition.attribute] !== condition.value) {
+                matching = true;
+              } else {
+                matchingAll = false;
+              }
+            }
+          }
+        }
+      });
+
+      if ((mustMatchAll && matchingAll) || (!mustMatchAll && matching)) {
+        console.log('Trigger power');
+      } else {
+        console.log('Power not triggered');
+      }
+    }
+  });
+}
+
+function positionCondition() {}
