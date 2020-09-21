cc385266fc856349571e3aeed30680e4e6c28ff9
Zak Rabe <zakRabe@gmail.com>
2020-09-21 02:14:46 -0400

    Import order

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,12 +1,12 @@
 import '@fortawesome/fontawesome-free/css/all.css';
 import React, { useState } from 'react';
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 import 'semantic-ui-css/semantic.min.css';
+import GoogleAnalytics from '../components/GoogleAnalytics';
 import UserContext from '../contexts/UserContext';
 import Routes from '../Routes';
 import './App.css';
 import AppHeader from './AppHeader';
-import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
-import GoogleAnalytics from '../components/GoogleAnalytics/GoogleAnalytics';
 
 const App: React.FC = () => {
   const [user, setUser] = useState(null);
