bb243d1b5ac11c4de131f5d20a67e7a72a45fbcf
Felipe Maia <felipe.cruz@radixeng.com>
2020-09-27 17:06:13 -0300

    started working on the individual card style
    added node-sass & further improved card and hover styles
    
    refactored card and playerzone components (empty slots)
    
    removed react template comments in index.html

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -1,24 +1,12 @@
 import { isEqual } from 'lodash';
 import React from 'react';
-
+import './styles.scss';
 import { CardProps } from './types';
 import CopyToClipboard from 'react-copy-to-clipboard';
 
 export const Card: React.FunctionComponent<CardProps> = (props) => {
   const { model, onClick, onHover, width, height } = props;
-
   const [isHovering, setIsHovering] = React.useState(false);
-
-  if (!model) {
-    return (
-      <img
-        width={width + 25}
-        height={height + 25}
-        style={{ margin: 0 }}
-        src="/images/normal/released/default.png"
-      ></img>
-    );
-  }
 
   const card = model || {
     id: 'default',
@@ -26,148 +14,257 @@ export const Card: React.FunctionComponent<CardProps> = (props) => {
     points: 0,
   };
 
-  const cardWrapperStyles = {
-    display: 'inline-flex',
-    borderRadius: '50%',
-    border: '1px solid silver',
-    background: 'linear-gradient(to right, white, #030303)',
-    padding: 3,
-    position: 'relative' as any,
-    margin: 'auto' as any,
-    cursor: onClick ? 'pointer' : 'default',
-  };
-  const cardBorderStyles = {
-    borderRadius: '50%',
-    border: '10px solid ' + card.colors[0],
-    position: 'relative' as 'relative', // ??
-  };
+  const colorToGradient = (color: string): string[] => {
+    if (color === 'BLACK') {
+      return ['#46535E', '#020203'];
+    }
 
-  const cardStyles = {
-    height,
-    width,
-    border: '8px inset rgba(255,255,255,.5)',
-    borderRadius: '50%',
-    backgroundImage: `url(/images/normal/released/${card.id}.jpg)`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    opacity: card.disabled ? 0.25 : 1,
-  };
+    if (color === 'BLUE') {
+      return ['#04BDFC', '#007AC0'];
+    }
 
-  const pointStyles = {
-    position: 'absolute' as any,
-    bottom: '10%',
-    right: '13%',
-    borderRadius: '50%',
-    fontSize: width / 6,
-    padding: width / 15,
-    width: width / 3,
-    height: width / 3,
-    lineHeight: `${width / 5}px`,
-    backgroundColor: card.colors[0],
-    color: 'white',
-    textAlign: 'center' as any,
-    boxSizing: 'border-box' as any,
-    textShadow: '-2px -2px rgba(0,0,0,.7)',
+    if (color === 'GREEN') {
+      return ['#00FB1F', '#009812'];
+    }
+
+    if (color === 'ORANGE') {
+      return ['#FBA419', '#D6720C'];
+    }
+
+    if (color === 'PURPLE') {
+      return ['#E542FC', '#AB0ABE'];
+    }
+
+    if (color === 'SILVER') {
+      return ['#D7E6E8', '#A4BFC9'];
+    }
+
+    if (color === 'YELLOW') {
+      return ['#F2FD0A', '#E2C701'];
+    }
+
+    return ['fff', 'fff'];
   };
 
-  const tableCellStyles = {
-    padding: 3,
-  };
+  const [gradientColorLight, gradientColorDark] = colorToGradient(card.colors[0]);
 
-  const renderCard = () => {
-    return (
-      <section
-        style={{
-          display: 'inline-flex',
-          userSelect: 'none',
-          position: 'relative',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-        }}
-        onMouseEnter={(e) => {
-          onHover && onHover(e);
-          setIsHovering(true);
-        }}
-        onMouseLeave={(e) => setIsHovering(false)}
-      >
-        <CopyToClipboard text={card.id.toString()}>
-          <div
-            style={{
-              cursor: 'pointer',
-              position: 'absolute',
-              bottom: '20%',
-              left: '8%',
-              fontSize: 16,
-              padding: 3,
-              background: 'white',
-            }}
-          >
-            <span>{card.id}</span>
-          </div>
-        </CopyToClipboard>
-        <div style={cardWrapperStyles} onClick={onClick}>
-          <div style={cardBorderStyles}>
-            <span style={pointStyles}>{card.points}</span>
-            <div
-              style={{
-                display: isHovering ? 'block' : 'none',
-                position: 'absolute',
-                backgroundColor: 'white',
-                padding: 3,
-              }}
-            >
-              <div>
-                <strong>{card.title}</strong>
-              </div>
-              <div>{card.description}</div>
-              <table
-                style={{
-                  borderTop: '3px solid rgba(0,0,0,.5)',
-                  borderBottom: '3px solid rgba(0,0,0,.5)',
-                }}
-              >
-                <tbody>
-                  <tr style={{}}>
-                    <td style={{ padding: 3 }}>Character</td>
-                    <td style={{ padding: 3, paddingLeft: 6 }}>
-                      {card.character}
-                    </td>
-                  </tr>
+  const handleOnMouseEnter = (e: any) => {
+    onHover && onHover(e);
+    setIsHovering(true);
+  }
 
-                  <tr style={{}}>
-                    <td style={{ padding: 3 }}>Types</td>
-                    <td style={{ padding: 3, paddingLeft: 6 }}>
-                      {card.types.join(' - ')}
-                    </td>
-                  </tr>
-                  <tr style={{}}>
-                    <td style={{ padding: 3 }}>Groups</td>
-                    <td style={{ padding: 3, paddingLeft: 6 }}>
-                      {card.groups.join(' - ')}
-                    </td>
-                  </tr>
-                  <tr style={{}}>
-                    <td style={{ padding: 3 }}>Base Points</td>
-                    <td style={{ padding: 3, paddingLeft: 6 }}>
-                      {card.basePoints}
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+  const handleOnMouseLeave = (e: any) => {
+    setIsHovering(false);
+  }
 
-              {card.modifiers?.map((mod) => {
-                return <div>{JSON.stringify(mod)}</div>;
-              })}
-            </div>
-            <div style={cardStyles}></div>
-          </div>
+  const renderHover = () => (
+    <div className={`info${isHovering ? ' show' : ''}`}>
+      <div className='header'>
+        <div className="title">
+          <strong>{card.title}</strong>
+          <CopyToClipboard text={card.id.toString()}>
+            <span className="debug">id: {card.id}</span>
+          </CopyToClipboard>
         </div>
-      </section>
-    );
-  };
+        <span>{card.description}</span>
+      </div>
+      <table>
+        <tbody>
+          <tr style={{}}>
+            <td>Character</td>
+            <td>{card.character}</td>
+          </tr>
+          <tr style={{}}>
+            <td>Types</td>
+            <td>
+              {card.types.join(', ')}
+            </td>
+          </tr>
+          <tr style={{}}>
+            <td>Groups</td>
+            <td>
+              {card.groups.join(', ')}
+            </td>
+          </tr>
+          <tr style={{}}>
+            <td>Base Points</td>
+            <td>{card.basePoints}</td>
+          </tr>
+        </tbody>
+      </table>
+      {card.modifiers?.length > 0 && (
+        <div className='modifiers'>
+          <span>Modifiers</span>
+          {card.modifiers?.map((mod) => 
+            <pre><code>{JSON.stringify(mod, null, 2)}</code></pre>
+          )}
+        </div>
+      )}
+    </div>
+  );
 
-  return renderCard();
+  const renderCardSVG = () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      width="200"
+      height="200"
+      viewBox="0 0 200 200"
+    >
+      <defs>
+        <linearGradient
+          id={`a${card.id}`}
+          y1="0.5"
+          x2="1"
+          y2="0.5"
+          gradientUnits="objectBoundingBox"
+        >
+          <stop offset="0" stop-color="#44759a" />
+          <stop offset="1" stop-color="#345f85" />
+        </linearGradient>
+        <radialGradient
+          id={`b${card.id}`}
+          cx="0.5"
+          cy="0.5"
+          r="0.477"
+          gradientUnits="objectBoundingBox"
+        >
+          <stop offset="0" stop-color="#eef4f8" />
+          <stop offset="0.948" stop-color="#eef4f8" />
+          <stop offset="1" stop-color="#b1cdde" />
+        </radialGradient>
+        <linearGradient
+          id={`d${card.id}`}
+          x1="0.885"
+          y1="0.816"
+          x2="0.069"
+          y2="0.231"
+          gradientUnits="objectBoundingBox"
+        >
+          <stop offset="0" stop-color={gradientColorLight} />
+          <stop offset="1" stop-color={gradientColorDark} />
+        </linearGradient>
+        <clipPath id={`e${card.id}`}>
+          <circle
+            cx="74"
+            cy="74"
+            r="74"
+            transform="translate(779 297)"
+            fill="#fff"
+          />
+        </clipPath>
+        <linearGradient
+          id={`f${card.id}`}
+          x1="0.885"
+          y1="0.816"
+          x2="-0.525"
+          y2="-0.292"
+          href={`#d${card.id}`}
+        />
+      </defs>
+      <circle cx="100" cy="100" r="100" fill={`url(#a${card.id})`} />
+      <circle
+        cx="99"
+        cy="99"
+        r="99"
+        transform="translate(1 1)"
+        fill={`url(#b${card.id})`}
+      />
+      <circle
+        cx="88"
+        cy="88"
+        r="88"
+        transform="translate(12 12)"
+        fill={`url(#a${card.id})`}
+      />
+      <g
+        transform="translate(14 14)"
+        fill="#afd2e3"
+        stroke="#a0b4c0"
+        stroke-width="2"
+      >
+        <circle cx="86" cy="86" r="86" stroke="none" />
+        <circle cx="86" cy="86" r="85" fill="none" />
+      </g>
+      <circle
+        cx="78"
+        cy="78"
+        r="78"
+        transform="translate(22 22)"
+        fill={`url(#d${card.id})`}
+      />
+      <g transform="translate(-753 -271)" clip-path={`url(#e${card.id})`}>
+        <image
+          preserveAspectRatio="xMidYMid slice"
+          width="148"
+          height="218"
+          transform="translate(779 271)"
+          href={`/images/normal/released/${card.id}.jpg`}
+        />
+      </g>
+      <g
+        transform="translate(113 111)"
+        fill="#fff"
+        stroke="#afd2e3"
+        stroke-width="4"
+      >
+        <circle cx="26" cy="26" r="26" stroke="none" />
+        <circle cx="26" cy="26" r="24" fill="none" />
+      </g>
+      <g
+        transform="translate(116 114)"
+        stroke="#8b9aa4"
+        stroke-width="1"
+        fill={`url(#f${card.id})`}
+      >
+        <circle cx="23" cy="23" r="23" stroke="none" />
+        <circle cx="23" cy="23" r="22.5" fill="none" />
+      </g>
+      <text
+        transform={`translate(${card.points > 9 ? '118' : '128'} 149)`}
+        fill="#afd2e3"
+        stroke="#8b9aa4"
+        stroke-width="1"
+        font-size="30"
+        font-family="Verdana-BoldItalic, Verdana"
+        font-weight="700"
+        font-style="italic"
+        textLength="40"
+      >
+        {card.points}
+      </text>
+      <path
+        d="M.144,146.983h0C.048,145.334,0,143.657,0,142A85.6,85.6,0,0,1,14.688,93.917,86.252,86.252,0,0,1,52.525,62.758,85.462,85.462,0,0,1,86,56h.47A86.1,86.1,0,0,0,50.025,73.863,85.841,85.841,0,0,0,.144,146.983Z"
+        transform="translate(18 -39)"
+        fill="#f9f9f9"
+        opacity="0.67"
+      />
+    </svg>
+  );
+
+  return (
+    <div
+      className={`card-wrapper${card.disabled ? ' disabled' : ''}`}
+      onClick={onClick}
+      onMouseEnter={handleOnMouseEnter}
+      onMouseLeave={handleOnMouseLeave}
+    >
+      {renderHover()}
+      {renderCardSVG()}
+    </div>
+  );
 };
 
 export default React.memo(Card, isEqual);
+
+// Todo
+
+// High:
+// add dynamic positioning of info box (hover) for cards at the bottom
+// check when `if (!model)` and if it should be replaced with this image inside the new svg wrapper
+
+// Medium:
+
+// Low:
+// decide how to handle multicolored cards (when `card.colors.length is > 1`) - currently we're using `card.colors[0]`
+// extract `colorToGradient` to a helpers file and also create a dictionary of color constants
